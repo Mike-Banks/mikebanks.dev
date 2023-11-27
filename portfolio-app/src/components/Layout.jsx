701f4d6f73b05331878d58679587a97da166bc0d
Mike Banks <mike.banks@focus21.io>
2023-11-27 14:24:19 -0500

    Setting global font

--- portfolio-app/src/components/Layout.jsx
+++ portfolio-app/src/components/Layout.jsx
@@ -1,13 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+import { Titillium_Web } from "next/font/google";
+ 
+const font = Titillium_Web({ subsets: ['latin'], weight: '300' })
+
 const Layout = ({children}) => {
   return (
-   	<>
+   	<div className={font.className}>
       <Navbar />
       <main>{children}</main>
 			<Footer />
-    </>
+    </div>
   )
 };
 
